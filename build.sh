f549aa19ca68b61f2f51a593ab7c8df96660e518
Samy Kaddour <141351381+SamyKadd@users.noreply.github.com>
2025-11-29 13:41:54 -0500

    Add build script for compiling C++ files

---
+++ build.sh
@@ -0,0 +1,4 @@
+set -e
+g++ -std=c++17 interrupts_EP_101081721_101258499.cpp -o interrupts_EP_101081721_101258499
+g++ -std=c++17 interrupts_RR_101081721_101258499.cpp -o interrupts_RR_101081721_101258499
+g++ -std=c++17 interrupts_EP_RR_101081721_101258499.cpp -o interrupts_EP_RR_101081721_101258499
